42d3be38b213ff2acfee9dd4f978b27e7020c27b
Anthonyxitadoo <164560821+Anthonyxitadoo@users.noreply.github.com>
2025-10-03 15:28:16 -0600

    Add files via upload

---
+++ ejercicio-if-else.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{ 
+int usuarioCorrecto = Anthony; // Usuario válido
+int contrasenaCorrecta = 20060924; // Contraseña válida
+int usuario, contrasena;
+int intentos = 3;
+
+    return 0;
+}
+
+
+
+
+
